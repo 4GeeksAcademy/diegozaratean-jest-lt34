1c3b937427ae31a831210b24c305cd4779fdd436
diego <diegozaratean@gmail.com>
2024-08-22 02:16:06 +0000

    finish react class

---
+++ file1.js
@@ -0,0 +1,7 @@
+import { sum, resta } from './file2.js'
+import unica from './file2.js'
+
+console.log('file 1')
+console.log(sum(5,7))
+console.log(resta(7,3))
+console.log(unica(2,8))
---
+++ file2.js
@@ -0,0 +1,22 @@
+let sum = (a,b)=>{
+    return a + b
+}
+
+let resta = (a,b)=>{
+    return a - b
+}
+
+let unica = (a,b)=>{
+    return a * b
+}
+
+let unica2 = (a,b)=>{
+    return a * b
+}
+
+export {
+    sum,
+    resta
+};
+
+export default unica
